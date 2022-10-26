02807bea39066d15f584dfbab8558cf7da653bb9
Shirleen <gathungushirleen@gmail.com>
2022-10-26 21:48:14 +0300

    changed placeholder input

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,7 +9,7 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getWeather(location:any){
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=828cebb23e2acd88cdf720ab896eb92a&query=London,United Kingdom'
+    'http://api.weatherstack.com/current?access_key=828cebb23e2acd88cdf720ab896eb92a&query=London,United Kingdom'
     );
 }
 
